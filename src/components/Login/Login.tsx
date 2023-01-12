9774a275cdf2c71e2e7a1226ecb87776f05592e0
mylescarswell@gmail.com <mylescarswell@gmail.com>
2023-01-12 17:27:30 -0500

    Login and register are connected to each other

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css'
 
-const Login = () => {
+
+
+  const Login: React.FC<any> = () => {
+ 
+
+    
+    const user = 
+    {
+        id:0,
+        username: "",
+        password:""
+    }
+ 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+ 
+     // we will use this to switch components - this is what lets us navigate to different
+        //components through events -no more manual URL changes
+        const navigate = useNavigate();
+
+                //when the user updates the username or password this will be called 
+        //the user/password state will get updated based on the name of the input thats changing 
+        const gatherInput = (input:any) => 
+        {
+            if(input.target.name === "username")
+            {
+            setUsername(input.target.value)
+            }
+            else{
+                setPassword(input.target.value)
+            }
+        }
+        const login =async () => 
+        {
+            //send an HTTP post request with axios, and store the response in a variable that we can use
+            const response = await axios.post("http://localhost:5555/data/login", {username, password})
+            
+            if(response.status === 202){
+                console.log(response)
+            //populate our objects from above with the incomming data from the server 
+            user.id = response.data.id;
+            user.username = response.data.username;
+            user.password = response.data.password;
+
+            // if the user logged in successfully, their id wont be 0;
+            if(user.id > 0)
+            {
+                navigate("/home")
+                //thanks to navigate, we can route to the home component automatically 
+            }
+          }
+        }
+
+ 
+ 
   return (
     <div className="Login">
       <section className="vh-100 gradient-custom">
@@ -18,20 +75,20 @@ const Login = () => {
 
                     <div className="form-outline form-white mb-2">
                       <label className="form-label" htmlFor="typeUserNameX">Username</label>
-                      <input type="text" id="typeUserNameX" className="form-control form-control-lg" />
+                      <input type="text" id="typeUserNameX" className="form-control form-control-lg" onChange={gatherInput} />
                     </div>
     
                     <div className="form-outline form-white mb-2">
                       <label className="form-label" htmlFor="typePasswordX">Password</label>
-                      <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                      <input type="password" id="typePasswordX" className="form-control form-control-lg" onChange={gatherInput} />
                     </div>
     
-                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                    <button className="btn btn-outline-light btn-lg px-5"  type="submit">Login</button>
 
                   </div>
 
                   <div>
-                    <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Register</a>
+                    <p className="mb-0">Don't have an account? <a href="/data/register" className="text-white-50 fw-bold">Register</a>
                     </p>
                   </div>
                 </div>
