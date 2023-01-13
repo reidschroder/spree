bdfe5b5b89c862d73fea6c4cb06f60eac2eb1bc5
mylescarswell@gmail.com <mylescarswell@gmail.com>
2023-01-13 17:06:20 -0500

    added the user profile interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
 import Register from './components/Register/Register';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/data/register" element={<Register/>}/>
           <Route path="/data/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </BrowserRouter>
     </div>
