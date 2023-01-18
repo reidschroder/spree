8a3c12400b81877c14bd65c5baeaf1fa4c9fb016
Reid Schroder <reidschroder2@gmail.com>
2023-01-17 22:02:06 -0500

    added address dao and address controller in intellij

--- server/Spoint/src/main/java/com/revature/controllers/AddressController.java
+++ server/Spoint/src/main/java/com/revature/controllers/AddressController.java
@@ -1,4 +1,40 @@
 package com.revature.controllers;
 
+
+import com.revature.daos.AddressDAO;
+import com.revature.models.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("")
 public class AddressController {
+
+
+    private AddressDAO aDAO;
+
+    @Autowired
+    public AddressController(AddressDAO aDAO) {
+        this.aDAO = aDAO;
+    }
+
+    //HTTP Requests
+
+    //Add Address
+    @PostMapping(value="/address")
+    public ResponseEntity addAddress(@RequestBody Address a) {
+
+        Address newAddress = aDAO.save(a);
+
+        if(newAddress == null){
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        return ResponseEntity.accepted().body(newAddress);
+
+    }
 }
